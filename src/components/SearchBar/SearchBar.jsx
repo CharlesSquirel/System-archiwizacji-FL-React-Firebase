624a6911291bacd7bf77a8c7bbf8d42c5de4c824
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-07-04 15:15:13 +0200

    Sorting checkbox view

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-import { StyledSearchBarWrapper, StyledSearchBarLabel, StyledSearchBarInput } from "./StyledSearchBar";
+import { StyledSearchBarWrapper, StyledLabel, StyledSearchBarInput, StyledCheckboxWrapper, StyledCheckBox } from "./StyledSearchBar";
 import { readfromDB } from "../../utils/firebase";
 
 const SearchBar = ({setCredentials }) => {
@@ -26,8 +26,27 @@ const SearchBar = ({setCredentials }) => {
   };
   return (
     <StyledSearchBarWrapper>
-      <StyledSearchBarLabel htmlFor="search">Wyszukaj</StyledSearchBarLabel>
+      <StyledLabel htmlFor="search">Wyszukaj</StyledLabel>
       <StyledSearchBarInput onChange={handleOnChange} name="search" id="search" value={query} type="text" placeholder="Wyszukaj" autoComplete="off" />
+      <StyledCheckboxWrapper>
+        <p>Sortuj według:</p>
+        <StyledCheckBox>
+            <label htmlFor="signaturecheckbox">sygnatury</label>
+            <input id="signaturecheckbox" type="checkbox" />
+        </StyledCheckBox>
+        <StyledCheckBox>
+            <label htmlFor="datecheckbox">daty</label>
+            <input id="datecheckbox" type="checkbox" />
+        </StyledCheckBox>
+        <StyledCheckBox>
+            <label htmlFor="ascendcheckbox">rosnąco</label>
+            <input id="ascendcheckbox" type="checkbox" />
+        </StyledCheckBox>
+        <StyledCheckBox>
+            <label htmlFor="descendcheckbox">malejąco</label>
+            <input id="descendcheckbox" type="checkbox" />
+        </StyledCheckBox>
+      </StyledCheckboxWrapper>
     </StyledSearchBarWrapper>
   );
 };
--- src/components/SearchBar/StyledSearchBar.jsx
+++ src/components/SearchBar/StyledSearchBar.jsx
@@ -2,10 +2,13 @@ import styled from "styled-components";
 
 export const StyledSearchBarWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
+  gap: 20px;
   width: 100%;
 `;
-export const StyledSearchBarLabel = styled.label`
+export const StyledLabel = styled.label`
   position: absolute;
   width: 1px;
   height: 1px;
@@ -19,3 +22,35 @@ export const StyledSearchBarLabel = styled.label`
 export const StyledSearchBarInput = styled.input`
   width: 50%;
 `;
+
+export const StyledCheckboxWrapper = styled.div`
+display: flex;
+gap: 15px;
+color: white;
+p {
+  font-size: 22px;
+}
+div:nth-of-type(2) {
+  position: relative;
+::after {
+  content: "";
+  background-color: white;
+  width: 2px;
+  height: 40px;
+  position: absolute;
+  right: -10px;
+}
+}
+`
+export const StyledCheckBox = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  label {
+    font-size: 18px;
+  }
+  input {
+    width: 30px;
+    height: 30px;
+  }
+`
