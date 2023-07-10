5f81506b856d5743a19bb683a33eeb83a92c4424
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-07-10 15:24:23 +0200

    validation edit

--- src/utils/yupvalidation.js
+++ src/utils/yupvalidation.js
@@ -3,7 +3,6 @@ import * as Yup from "yup";
 export const validationSchema = Yup.object().shape({
   signature: Yup.string().required("Pole jest wymagane!"),
   date: Yup.string().required("Pole jest wymagane!")
-  .test("dla wartości mniejszych od 10 input musi zaczynać się od 0", "Pole musi zaczynać się od 0", value => !value.match(/^[1-9]+\./))
   .test("długość inputa jest równa 10", "Pole musi zawierać 10 znaków", value => value.length === 10),
   description: Yup.string(),
   tags: Yup.string()
