ee67542d49a00ae4c1d450c8ec6b6fb8cbe29fd6
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-08-02 14:00:30 +0200

    Deleting unused style

--- src/components/_DataLists/DataListArchive/StyledDataList.jsx
+++ src/components/_DataLists/DataListArchive/StyledDataList.jsx
@@ -78,24 +78,6 @@ export const StyledButtonBox = styled.div`
   align-self: center;
   gap: 5px;
 `;
-export const StyledButtonEdit = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  align-self: center;
-  gap: 5px;
-  width: 68px;
-  height: 28px;
-  padding: 5px;
-  border-radius: 5px;
-  background: var(--primary);
-  color: var(--white);
-  font-weight: 600;
-  font-size: 15px;
-`;
-export const StyledButtonDelete = styled(StyledButtonEdit)`
-  /* margin-left: 5px; */
-`;
 
 export const StyledDataButton = styled.button`
   display: grid;
